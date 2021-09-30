e9e1739bbbbda9ca18ed18089d96f56a9ab07c46
Admin <scamer10@uoguelph.ca>
2021-09-29 20:42:34 -0700

    first commit

---
+++ building.py
@@ -0,0 +1,16 @@
+class Building:
+
+    def __init__(self, price, address, building_type, year_built=0, floors=0, postal_code='', mls_number=0,
+                 description=''):
+        self.price = price
+        self.address = address
+        self.build_type = building_type
+        self.year_built = year_built
+        self.floors = floors
+        self.postal_code = postal_code
+        self.mls_number = mls_number
+        self.description = description
+
+
+
+
